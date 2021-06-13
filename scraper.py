898a255e15167ba5cb9b06a79f0c4ea6a5b56212
Aryan Rajput <46697997+AryanRajput78@users.noreply.github.com>
2021-06-13 17:07:50 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,14 +1,17 @@
+#Import all the libraries. I have used selenium to individually load and capture data from each file.
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 import pandas as pd
 import csv
 
+#Set-up all the elements of opening and closing the webpage.
 driver = webdriver.Chrome(executable_path='C:\\Users\\ARYAN\\Downloads\\chromedriver')
 url = 'https://www.amazon.in/alm/category/?_encoding=UTF8&almBrandId=ctnow&node=4859479031&pf_rd_p=cefc1b78-88f9-45f8-bbd6-24e2f5e8980d&pf_rd_r=HNCTW1AQC1126WYHERZ8&pf_rd_s=nowstore-desktop-food-content-1&pf_rd_t=SubnavFlyout&ref_=sn_gfs_co_nowstore-subnav-food_Ce_2'
 a = ActionChains(driver)
 driver.get(url)
 driver.maximize_window()
 
+#Short loop to find all the categories on the home page.
 categories = driver.find_elements_by_class_name('a-size-large')
 print('Loading the page...')
 for cat in categories:
@@ -16,12 +19,14 @@ for cat in categories:
     cat.click()
     driver.implicitly_wait(1)
 
+#Main loop where all the items are found, stored and printed.
 print('Finding the elements...')
 for cat, l in zip(categories, range(0, len(categories))):
     temp = cat.text.strip().split('See')[0]
     print("Category: {}".format(temp))
     head = driver.find_element_by_id('anonCarousel{}'.format(l+1))
     child = head.find_elements_by_xpath('//*[@id="anonCarousel{}"]/ol'.format(l+1))
+    #As there are many categories we are looping over, I've used the ID to separate each category with its different items.
     catalogue = []
     for c in child:
         prod = []
@@ -48,6 +53,7 @@ for cat, l in zip(categories, range(0, len(categories))):
             driver.switch_to.window(driver.window_handles[1])
             driver.implicitly_wait(5)
             
+            #We've used the try-except blocks for avoiding any errors or any missing entries.
             try:
                 id = driver.find_element_by_xpath('//*[@id="productDetails_detailBullets_sections1"]/tbody/tr[1]/td')
             except:
@@ -70,7 +76,7 @@ for cat, l in zip(categories, range(0, len(categories))):
             except:
                 place = 'None'
 
-            
+            #Pardon the naming, this is just to make it easier to understand.
             a = id.text.strip()
             b = Title.text.strip()
             if MRP != 'None': 
